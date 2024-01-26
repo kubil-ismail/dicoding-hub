d5e4c271c6d79a196f2307f8ece50267f3bb2200
Bilkis Ismail <bilkis@imajin.id>
2024-01-26 15:14:16 +0700

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -102,7 +102,7 @@ function Login() {
           disabled={isLoading}
           fullWidth
         >
-          {isLoading ? 'Loading...' : 'Loginss'}
+          {isLoading ? 'Loading...' : 'Login'}
         </Button>
 
         <Box display="flex" justifyContent="center" mt={2}>
